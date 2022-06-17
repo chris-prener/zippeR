a1ce9c1347896766e2fb9a1884201ad1167c36a1
Christopher Prener <chris.prener@slu.edu>
2022-06-17 09:09:56 -0500

    remove example to see if that passes

--- R/zi_crosswalk.R
+++ R/zi_crosswalk.R
@@ -28,13 +28,6 @@
 #'
 #' @return A \code{tibble} with ZCTAs appended.
 #'
-#' @examples
-#' # sample data
-#' zips <- data.frame(id = c(1:3), ZIP = c("63139", "63108", "00501"))
-#'
-#' # crosswalk
-#' zi_crosswalk(zips, input_zip = ZIP)
-#'
 #' @export
 zi_crosswalk <- function(.data, input_zip, dict = "UDS 2021", dict_zip = "ZIP",
                          dict_zcta = "ZCTA", style = "zcta5"){
@@ -74,25 +67,19 @@ zi_crosswalk <- function(.data, input_zip, dict = "UDS 2021", dict_zip = "ZIP",
   }
 
   # verify formatting for ZIPs
-  test <- .data[[in_zipQN]]
-
-  valid <- zi_validate(x = test)
+  valid <- zi_validate(x = .data[[in_zipQN]])
 
   if (valid == FALSE){
     stop(paste0("Input ZIP Code data in the '", in_zipQN, "' column are invalid. Please use 'zi_validate()' with the 'verbose = TRUE' option to investgiate further. The 'zi_repair()' function may be used to address isses."))
   }
 
-  test <- dict[[dict_zipQN]]
-
-  valid <- zi_validate(x = test)
+  valid <- zi_validate(x = dict[[dict_zipQN]])
 
   if (valid == FALSE){
     stop(paste0("Dictionary ZIP Code data in the '", dict_zipQN, "' column are invalid. Please use 'zi_validate()' with the 'verbose = TRUE' option to investgiate further. The 'zi_repair()' function may be used to address isses."))
   }
 
-  test <- dict[[dict_zctaQN]]
-
-  valid <- zi_validate(x = test, style = style)
+  valid <- zi_validate(x = dict[[dict_zctaQN]], style = style)
 
   if (valid == FALSE & style == "zcta5"){
     stop(paste0("Dictionary ZCTA data in the '", dict_zctaQN, "' column are invalid. Please use 'zi_validate()' with the 'verbose = TRUE' option to investgiate further. The 'zi_repair()' function may be used to address isses."))
@@ -102,9 +89,7 @@ zi_crosswalk <- function(.data, input_zip, dict = "UDS 2021", dict_zip = "ZIP",
 
     dict[[dict_zctaQN]] <- substr(dict[[dict_zctaQN]], 1, 3)
 
-    test <- dict[[dict_zctaQN]]
-
-    valid <- zi_validate(x = test, style = "zcta3")
+    valid <- zi_validate(x = dict[[dict_zctaQN]], style = style)
 
     if (valid == FALSE){
       stop(paste0("Dictionary ZCTA data in the '", dict_zctaQN, "' column are invalid. Please use 'zi_validate()' with the 'verbose = TRUE' option to investgiate further. The 'zi_repair()' function may be used to address isses."))
